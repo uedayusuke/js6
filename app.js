a18191a5afe3eaa2d049904cf829129e8c4571e8
uedayusuke <uedayusuke1107@gmail.com>
2020-04-09 11:14:16 +0900

    [Finish]6-6

---
+++ app.js
@@ -0,0 +1,28 @@
+$(function(){
+	$(".box1").mouseover(function(){
+		$(".box1").css({"background-color": "#0000FF"});
+	});
+
+	$(".box1").mouseout(function(){
+		$(".box1").css({"background-color": "#FF0000"});
+	});
+});
+
+$(function(){
+	$(".box2").mouseover(function(){
+		$(".box2").addClass("box2-ext");
+	});
+	$(".box2").mouseout(function(){
+		$(".box2").removeClass("box2-ext");
+	});
+});
+
+$(function(){
+	$(".box3").on("click",function(){
+		$(".box3").addClass("box3-ext");
+	});
+
+	$(".box3").mouseout(function(){
+		$(".box3").removeClass("box3-ext");
+	});
+})
